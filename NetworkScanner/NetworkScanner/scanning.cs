691755643a72db9491178594eef2063b99bb5dd8
fasihrohan <77568715+fasihrohan@users.noreply.github.com>
2023-01-11 11:02:55 +0500

    Add files via upload

---
+++ NetworkScanner/NetworkScanner/scanning.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Net;
+using System.Management;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+
+namespace NetworkScanner
+{
+    public class scanning
+    {
+    }
+}
